fdf4fb9ae21fd63bbb328853a6c04f2f60ee6e0c
soumya24july <soumya24july@gmail.com>
2024-06-20 12:55:33 +0530

    My first commit

---
+++ src/test/java/practice.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {9,7,5,4};
+		
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]<a[j]) {
+					
+					
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+				
+			}
+		}
+		
+		for(int i=0;i<a.length;i++) {
+			if(i==3)
+			{
+				System.out.println("4th Maximum "+a[i]);
+			}
+		}
+	}
+
+}
